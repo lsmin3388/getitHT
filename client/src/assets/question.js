0e5633dd2378110aaf8035736382a6b676911871
toothlessdev <toothless042@gmail.com>
2023-05-26 22:35:17 +0900

    Test for QuestionPage API

--- client/src/assets/question.js
+++ client/src/assets/question.js
@@ -1,26 +1,146 @@
-export default question = [
+let questionDefault = [
     {
-        qString: "질문 ????",
+        qString: "질문 : 도수가 높은 술이 좋다.",
         qWeight: [
             {
                 //매우 아니다
-                type: "raw",
-                weight: 5,
+                type: "soju",
+                weight: 1,
             },
             {
                 //아니다
-                type: "clear",
-                weight: 5,
+                type: "soju",
+                weight: 2,
             },
             {
                 //보통
                 type: "soju",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "soju",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "soju",
                 weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 꾸덕한 음료를 좋아한다",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "raw",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "raw",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "raw",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "raw",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "raw",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 평소에 숙취를 잘 느끼지 못한다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "raw",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "raw",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "raw",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "raw",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "raw",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 칵테일을 좋아한다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "fruit",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "fruit",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "fruit",
+                weight: 3,
             },
             {
                 //그렇다
                 type: "fruit",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "fruit",
                 weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 고소한 맛을 좋아한다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "clear",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "clear",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "clear",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "clear",
+                weight: 4,
             },
             {
                 //매우 그렇다
@@ -29,4 +149,336 @@ export default question = [
             },
         ],
     },
+    {
+        qString: "질문 : 누룩맛을 좋아한다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "raw",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "raw",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "raw",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "raw",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "raw",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 소주를 좋아한다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "soju",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "soju",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "soju",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "soju",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "soju",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 슬을 마셨을 때, 타는 듯한 목넘김이 좋다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "soju",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "soju",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "soju",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "soju",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "soju",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 숙취가 없는 술이 좋다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "clear",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "clear",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "clear",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "clear",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "clear",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 과일향을 좋아한다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "fruit",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "fruit",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "fruit",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "fruit",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "fruit",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 막걸리를 좋아한다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "raw",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "raw",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "raw",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "raw",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "raw",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 깔끔한 술이 좋다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "clear",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "clear",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "clear",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "clear",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "clear",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 보기에 예쁜 술이 맛도 좋다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "fruit",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "fruit",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "fruit",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "fruit",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "fruit",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 위스키를 좋아한다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "soju",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "soju",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "soju",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "soju",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "soju",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 사케를 좋아한다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "clear",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "clear",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "clear",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "clear",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "clear",
+                weight: 5,
+            },
+        ],
+    },
+    {
+        qString: "질문 : 음료수같은 술이 좋다.",
+        qWeight: [
+            {
+                //매우 아니다
+                type: "fruit",
+                weight: 1,
+            },
+            {
+                //아니다
+                type: "fruit",
+                weight: 2,
+            },
+            {
+                //보통
+                type: "fruit",
+                weight: 3,
+            },
+            {
+                //그렇다
+                type: "fruit",
+                weight: 4,
+            },
+            {
+                //매우 그렇다
+                type: "fruit",
+                weight: 5,
+            },
+        ],
+    },
 ];
+
+export default questionDefault;
--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -8,59 +8,16 @@ import { increaseClear, increaseFruit, increaseRaw, increaseSoju } from "../stor
 import "./Question.css";
 
 export default function Question({ index, qString, qWeightArray }) {
-    const question = useSelector((state) => state.question.value);
-    const dispatch = useDispatch();
-
-    function onRadioChangeHandler(e) {
-        // console.log(this.value);
-        console.log(document.querySelector(`input[name='question${index}']:checked`).value);
-        increaseWeight(qWeightArray[this.value].type, qWeightArray[this.value].weight);
-
-        //이전에 체크된값에 대한 가중치 제거
-    }
-
-    function increaseWeight(type, weight) {
-        console.log(type);
-        console.log(weight);
-        switch (type) {
-            case "raw":
-                dispatch(increaseRaw(weight));
-                break;
-            case "clear":
-                dispatch(increaseClear(weight));
-                break;
-            case "soju":
-                dispatch(increaseSoju(weight));
-                break;
-            case "fruit":
-                dispatch(increaseFruit(weight));
-                break;
-        }
-    }
-
-    useEffect(() => {
-        const radioElements = document.querySelectorAll(`input[name='question${index}']`);
-        for (const radioElement of radioElements) {
-            radioElement.addEventListener("change", onRadioChangeHandler);
-        }
-
-        return () => {
-            for (const radioElement of radioElements) {
-                radioElement.removeEventListener("change", onRadioChangeHandler);
-            }
-        };
-    }, []);
-
     return (
         <div className="question-wrapper">
             <div className="question-container">
                 <h3>{qString}</h3>
                 <div key={`inline-radio`} className="question-radio-container">
-                    <input type="radio" name={`question${index}`} value="0" />
-                    <input type="radio" name={`question${index}`} value="1" />
-                    <input type="radio" name={`question${index}`} value="2" />
-                    <input type="radio" name={`question${index}`} value="3" />
-                    <input type="radio" name={`question${index}`} value="4" />
+                    <input type="radio" name={`question${index}`} value="0" id={`q${index}-0`} />
+                    <input type="radio" name={`question${index}`} value="1" id={`q${index}-1`} />
+                    <input type="radio" name={`question${index}`} value="2" id={`q${index}-2`} />
+                    <input type="radio" name={`question${index}`} value="3" id={`q${index}-3`} />
+                    <input type="radio" name={`question${index}`} value="4" id={`q${index}-4`} />
                 </div>
             </div>
         </div>
